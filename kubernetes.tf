8f1a5d4e28aae41ddda911e20a49a1777ff381cf
cnetsec <cursosnetsec@gmail.com>
2022-12-05 01:21:18 -0300

    terrakind

---
+++ kubernetes.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    kind = {
+      source = "unicell/kind"
+      version = "0.0.2-u2"
+    }
+  }
+}
+resource "kind_cluster" "default" {
+    name = "test-cluster"
+}
+
+resource "null_resource" "kubernetesgoat" {
+
+ provisioner "local-exec" {
+    command = "chmod +x setup-kubernetes-goat.sh"
+  }
+ provisioner "local-exec" {
+    command = "bash setup-kubernetes-goat.sh"
+  }
+ provisioner "local-exec" {
+    command = "bash access-kubernetes-goat.sh"
+  }
+}
